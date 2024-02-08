fa86e853dc52b5424a80d813c69e565984a1724d
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-08 17:23:58 +0100

    Style the hero page

--- fitness-app/src/App.js
+++ fitness-app/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
-
-
-
-
-
+import programs from './components/programes/programs';
 
 function App() {
   return (
     <div className="App">
         <Hero/>
+        <programes/>
     </div>
   );
 }
