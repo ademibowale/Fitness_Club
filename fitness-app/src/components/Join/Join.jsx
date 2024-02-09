60896a570012f4851a22212821cd34a8dca78e2c
ademibowale <ademibowaleadegboye@gmail.com>
2024-02-09 23:21:53 +0100

    Implement the email submision using @emailjs  and styles the email submision

--- fitness-app/src/components/Join/Join.jsx
+++ fitness-app/src/components/Join/Join.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import './Join.css'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
 
 const Join = () => {
+   const form = useRef()
+    const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_hv4jt5m', 'template_xg6nlxi', form.current, {
+        publicKey: 'Nd2dHNR1SNbnoOZQY',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className='Join' id='Join-Us'>
         <div className='left-j'>
@@ -15,13 +33,14 @@ const Join = () => {
         <span className='stroke-text'> WITH US ?</span>
      </div> 
      </div>
-    <div className='right-j'></div>
-    <form action='' className='email-container'>
+    <div className='right-j'>
+    <form  ref={form}  className='email-container' onSubmit={sendEmail}>
         <input type='email' name='user_email' placeholder='Enter your email address to join us'/>
+        <textarea className='email-container'  name='message' placeholder='Enter your message'></textarea>
         <button className='btn-j'>JOIN US</button>
     </form>
 
-      
+    </div>
     </div>
   )
 }
